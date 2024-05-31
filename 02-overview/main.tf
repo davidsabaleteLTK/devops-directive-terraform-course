fb55fb1ea5aa987285df4d98b509530f0005aecb
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2024-05-31 19:05:02 +0200

    create vpc and subnet

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -11,7 +11,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
 }
